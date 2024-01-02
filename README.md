1cf4b0c404e0113a6fff266abd54bce2f16799a5
aks <aks@cforge.org>
2024-01-03 01:29:32 +0400

    imported

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mod_piper_tts
+<p>
+  Freeswitch TTS module, provides a simple frontend for <a href="https://github.com/rhasspy/piper" target="_blank">piper</a>.<br>
+</p>
+
+### Usage example
+```XML
+<extension name="tts-test">
+    <condition field="destination_number" expression="^(3333)$">
+        <action application="answer"/>
+        <action application="speak" data="piper|en|Hello world!"/>
+        <action application="sleep" data="1000"/>
+        <action application="hangup"/>
+    </condition>
+</extension>
+
+```
---
+++ sources/mod_piper_tts.h
@@ -0,0 +1,54 @@
+/**
+ * (C)2023 aks
+ * https://akscf.me/
+ * https://github.com/akscf/
+ **/
+#ifndef MOD_PIPER_TTS_H
+#define MOD_PIPER_TTS_H
+
+#include <switch.h>
+#include <switch_stun.h>
+#include <stdint.h>
+#include <string.h>
+
+#ifndef true
+#define true SWITCH_TRUE
+#endif
+#ifndef false
+#define false SWITCH_FALSE
+#endif
+
+#define VERSION                 "1.0"
+#define PIPER_FILE_ENCODING     "wav"
+
+typedef struct {
+    switch_mutex_t          *mutex;
+    switch_hash_t           *models;
+    const char              *tmp_path;
+    const char              *cache_path;
+    const char              *piper_bin;
+    const char              *piper_opts;
+    uint8_t                 fl_cache_disabled;
+    uint8_t                 fl_voice_as_lang_code;
+} globals_t;
+
+typedef struct {
+    char                    *lang;
+    char                    *model;
+} piper_model_info_t;
+
+typedef struct {
+    piper_model_info_t      *model_info;
+    switch_memory_pool_t    *pool;
+    switch_file_handle_t    *fhnd;
+    char                    *language;
+    char                    *voice;
+    char                    *dst_fname;
+    uint32_t                samplerate;
+    uint32_t                channels;
+} tts_ctx_t;
+
+piper_model_info_t *piper_lookup_model(const char *lang);
+
+
+#endif
